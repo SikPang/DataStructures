096b05dbec0c6b182443d6f9753c2492beb58420
SikPang <87380790+SikPang@users.noreply.github.com>
2023-03-22 09:50:11 +0900

    vector v0.1

--- C_DynamicArray/DynamicArray.c
+++ C_DynamicArray/DynamicArray.c
@@ -1,2 +1,78 @@
 #include "DynamicArray.h"
 
+void initialize(vector* vec)
+{
+	vec->data = NULL;
+	vec->size = 0;
+	vec->capacity = 0;
+}
+
+void reserve(vector* vec, int size)
+{
+	if (size <= vec->size)
+		return;
+	
+	vec->data = (int*)malloc(size * sizeof(int));
+	vec->size = size;
+}
+
+static void realloc_double(vector* vec)
+{
+	if (vec->capacity == 0)
+		vec->capacity = 1;
+	else
+		vec->capacity *= 2;
+
+	int* new_data = (int*)malloc(vec->capacity * sizeof(int));
+
+	for (int i = 0; i < vec->size; ++i)
+		new_data[i] = vec->data[i];
+
+	free(vec->data);
+	vec->data = new_data;
+}
+
+void push_back(vector* vec, int data)
+{
+	if (vec->size == vec->capacity)
+		realloc_double(vec);
+	
+	vec->data[vec->size] = data;
+	++vec->size;
+}
+
+void pop_back(vector* vec, int data)
+{
+	--vec->size;
+}
+
+void insert(vector* vec, int index, int data)
+{
+	if (index >= vec->size)
+		return;
+
+	if (vec->size == vec->capacity)
+		realloc_double(vec);
+
+	for (int i = vec->size - 1; i >= index; --i)
+		vec->data[i + 1] = vec->data[i];
+	
+	vec->data[index] = data;
+	++vec->size;
+}
+
+void erase(vector* vec, int index)
+{
+	if (index >= vec->size)
+		return;
+
+	for (int i = index + 1; i < vec->size; ++i)
+		vec->data[i] = vec->data[i - 1];
+	--vec->size;
+}
+
+void clear(vector* vec)
+{
+	free(vec->data);
+	initialize(vec);
+}
--- C_DynamicArray/DynamicArray.h
+++ C_DynamicArray/DynamicArray.h
@@ -3,6 +3,14 @@
 
 #include <stdlib.h>
 
+typedef struct vector
+{
+	int* data;
+	int size;
+	int capacity;
+} vector;
 
+void initialize(vector* vec);
+void reserve(vector* vec, int size);
 
 #endif
